7e1ed46af0be6c714bdeb4af3d91e4da49db6866
Starlight00-L <93304658+Starlight00-L@users.noreply.github.com>
2024-05-11 19:55:21 -0700

    Started Wave spawner and added player sprites
    Pushed so player sprites could be included

---
+++ Scripts/Wave.gd
@@ -0,0 +1,7 @@
+class_name Wave
+extends Resource
+
+@export var wave_name : String
+@export var enemies : Array[String]
+@export var points : int = 0
+@export var spawn_delay : float = 1
---
+++ Scripts/Wave_Controller.gd
@@ -0,0 +1,4 @@
+class_name Wave_Manager
+extends Node2D
+
+@export var enemies : Array[Wave]
