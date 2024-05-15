4f9717140c6e32e5dd87c8840937781b0bbbd25c
Starlight00-L <93304658+Starlight00-L@users.noreply.github.com>
2024-05-14 18:01:54 -0700

    Enemy obstacle avoidence
    Enemy can avoid obstacles to a certain degree

--- Scripts/enemy_movement.gd
+++ Scripts/enemy_movement.gd
@@ -4,11 +4,17 @@ extends Node
 enum states{STRAFING, MOVING}
 
 @onready var move_timer = $"../Move Timer"
+@onready var ray_check_timer = $"../Ray check"
+@onready var direction_ray = $"../Direction Holder/Direction Ray"
 
 var moving_states = states.MOVING
 
 @export var max_speed : float = 5
 @export var accel : float = 0
+@export var rot_speed : float = 2
+var angle_cone_of_vision := deg_to_rad(161)
+var max_view_distance := 30
+var angle_between_rays := deg_to_rad(40)
 var move_dir : Vector2
 
 @export var strafe_move_time_max : float = 3
@@ -21,8 +27,10 @@ var parent
 
 func _ready():
 	parent = get_parent()
+	generate_raycasts()
 
 func _physics_process(delta):
+	check_movement_direction(delta)
 	movement(delta)
 	parent.move_and_collide(parent.velocity)
 
@@ -31,7 +39,18 @@ func set_move_dir(new_pos : Vector2):
 		return
 	
 	var new_dir = new_pos - parent.position
+	direction_ray.rotate(new_dir.angle())
 	move_dir = new_dir.normalized()
+
+func check_movement_direction(delta): #moves direction when detecting an obstacle in front of it
+	for ray in direction_ray.get_children():
+		if ray is RayCast2D && ray.is_colliding():
+			if ray.rotation > 0:
+				direction_ray.rotate(delta * -rot_speed)
+			elif ray.rotation < 0:
+				direction_ray.rotate(delta * rot_speed)
+			move_dir = get_ray_dir(direction_ray)
+			break
 
 func movement(delta):
 	parent.velocity += move_dir * accel
@@ -48,6 +67,19 @@ func check_rays():
 			move_dir = -get_ray_dir(r)
 			return
 
+func generate_raycasts() -> void: #Makes raycasts onto direction ray in a cone for obstacle avoidence
+	var ray_count := angle_cone_of_vision / angle_between_rays
+	
+	for index in ray_count:
+		var ray := RayCast2D.new()
+		var angle := angle_between_rays * (index - ray_count / 2.0)
+		ray.rotation = angle
+		ray.target_position.y = max_view_distance
+		ray.set_collision_mask_value(1, false)
+		ray.set_collision_mask_value(2, true)
+		direction_ray.add_child(ray)
+		ray.enabled = true
+
 func get_ray_dir(ray):
 	var r_global_origin = ray.to_global(Vector2.ZERO)
 	var r_global_cast_to_endpoint = ray.get_child(0).global_position
@@ -61,13 +93,8 @@ func _on_stop_range_area_entered(area):
 	moving_states = states.STRAFING
 	pause = true
 	strafe()
+	ray_check_timer.start()
 	move_timer.start()
-
-func _on_ray_check_timeout():
-	check_rays()
-
-func _on_stop_range_area_exited(area):
-	pause = false
 
 func _on_move_timer_timeout():
 	move_timer.wait_time = randf_range(strafe_move_time_min, strafe_move_time_max)
@@ -75,5 +102,9 @@ func _on_move_timer_timeout():
 
 func _on_chase_range_area_exited(area):
 	pause = false
+	ray_check_timer.stop()
 	move_timer.stop()
 	moving_states = states.MOVING
+
+func _on_ray_check_timeout():
+	check_rays()
